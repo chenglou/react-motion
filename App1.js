5a40b46ce05c7239fcc085623278bc3488528129
Cheng Lou <chenglou92@gmail.com>
2015-05-29 17:00:03 -0400

    Further refactor into 2 experiments

---
+++ App1.js
@@ -0,0 +1,157 @@
+let React = require('react');
+let stepper = require('./stepper');
+
+let Spring = React.createClass({
+  propTypes: {
+    tension: React.PropTypes.number,
+    friction: React.PropTypes.number,
+    // initialValue: React.PropTypes.number,
+    value: React.PropTypes.number.isRequired,
+    onValueChange: React.PropTypes.func,
+  },
+
+  getDefaultProps: function() {
+    return {
+      tension: 140,
+      friction: 16,
+    };
+  },
+
+  getInitialState: function() {
+    return {
+      v: 0,
+      currValue: this.props.value,
+      isRafing: true,
+    };
+  },
+
+  componentWillReceiveProps: function(nextProps) {
+    if (!this.state.isRafing) {
+      this.setState({isRafing: true});
+      this.raf();
+    }
+  },
+
+  raf: function() {
+    requestAnimationFrame(() => {
+      let {currValue, v} = this.state;
+      let {tension, friction, value, onValueChange} = this.props;
+
+      let [newCurrValue, newV] =
+        stepper(currValue == null ? value : currValue, v, value, tension, friction);
+
+      if (newV === v && newCurrValue === currValue) {
+        this.setState({isRafing: false});
+        return;
+      }
+
+      this.setState(() => {
+        onValueChange && onValueChange(newCurrValue);
+        return {
+          currValue: newCurrValue,
+          v: newV,
+        };
+      });
+
+      this.raf();
+    });
+  },
+
+  componentDidMount: function() {
+    this.raf();
+  },
+
+  render: function() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      mouseX: 0,
+      mouseY: 0,
+      springs: [
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+        [0, 0],
+      ],
+    };
+  },
+
+  handleMouseMove: function(e) {
+    if (e.nativeEvent.which === 1) {
+      this.setState({
+        mouseX: e.pageX,
+        mouseY: e.pageY,
+      });
+    }
+  },
+
+  // componentDidMount: function() {
+  //   let asd = () => {
+  //     let s = JSON.stringify(this.state);
+  //     requestAnimationFrame(() => {
+  //       // if (JSON.stringify(this.state) !== s) {
+  //         // console.log('up');
+  //         this.forceUpdate();
+  //       // }
+  //       asd();
+  //     });
+  //   }
+
+  //   asd();
+  // },
+
+  handleValueChange: function(idx, pos, value) {
+    this.state.springs[idx][pos] = value;
+    this.setState({
+      springs: this.state.springs,
+    });
+  },
+
+  render: function() {
+    let {mouseX, mouseY, springs} = this.state;
+    let box = {
+      width: 500,
+      height: 600,
+      backgroundColor: 'lightgray',
+    };
+
+    let s = {
+      position: 'absolute',
+      border: '1px solid black',
+      borderRadius: 99,
+      width: 50,
+      height: 50,
+      backgroundColor: 'red',
+    };
+
+    return (
+      <div onMouseMove={this.handleMouseMove} onMouseDown={this.handleMouseMove} style={box}>
+        {springs.map(([x, y], i) => {
+          let [destX, destY] = i === 0 ? [mouseX, mouseY] : springs[i - 1];
+          return (
+            <Spring key={i}  value={destX} onValueChange={this.handleValueChange.bind(null, i, 0)}>
+              <Spring  value={destY} onValueChange={this.handleValueChange.bind(null, i, 1)}>
+                <div style={{
+                  ...s,
+                  WebkitTransform: `translate3d(${x}px, ${y}px, 0)`,
+                  zIndex: springs.length - i,
+                }} />
+              </Spring>
+            </Spring>
+          );
+        })}
+      </div>
+    );
+  }
+});
+
+module.exports = App;
---
+++ App2.js
@@ -0,0 +1,139 @@
+let React = require('react');
+let stepper = require('./stepper');
+
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      mouseX: 0,
+      mouseY: 0,
+      springs: [
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+      ],
+    };
+  },
+
+  rafId: null,
+
+  repeatSetState: function(f) {
+    if (this.rafId != null) {
+      cancelAnimationFrame(this.rafId);
+      this.rafId = null;
+    }
+
+    var state = f();
+    if (!state) {
+      return;
+    }
+
+    this.setState(state);
+    let loop = () => {
+      this.rafId = requestAnimationFrame(() => {
+        let state = f();
+        if (state) {
+          this.setState(state);
+          loop();
+        }
+      });
+    };
+
+    loop();
+  },
+
+  // handleMouseMove: function(e) {
+  //   if (e.nativeEvent.which === 0) {
+  //     return;
+  //   }
+
+  //   let {pageX, pageY} = e;
+  //   this.repeatSetState(() => {
+  //     let {springs} = this.state;
+  //     let newSprings = springs.map(([x, y, vx, vy], i) => {
+  //       let [destX, destY] = i === 0 ? [pageX, pageY] : springs[i - 1];
+  //       let [newX, newVx] = stepper(x, vx, destX, 140, 16);
+  //       let [newY, newVy] = stepper(y, vy, destY, 140, 16);
+
+  //       return [newX, newY, newVx, newVy];
+  //     });
+
+  //     let newState = {
+  //       mouseX: pageX,
+  //       mouseY: pageY,
+  //       springs: newSprings,
+  //     };
+  //     if (JSON.stringify(this.state) !== JSON.stringify(newState)) {
+  //       return newState;
+  //     }
+  //     return null;
+  //   });
+  // },
+
+  handleMouseMove: function(e) {
+    if (e.nativeEvent.which === 0) {
+      return;
+    }
+
+    this.setState({
+      mouseX: e.pageX,
+      mouseY: e.pageY,
+    });
+  },
+
+  componentDidMount: function() {
+    let loop = () => {
+      requestAnimationFrame(() => {
+        let {springs, mouseX, mouseY} = this.state;
+        let newSprings = springs.map(([x, y, vx, vy], i) => {
+          let [destX, destY] = i === 0 ? [mouseX, mouseY] : springs[i - 1];
+          let [newX, newVx] = stepper(x, vx, destX, 120, 16);
+          let [newY, newVy] = stepper(y, vy, destY, 120, 16);
+
+          return [newX, newY, newVx, newVy];
+        });
+
+        this.setState({
+          springs: newSprings,
+        });
+        loop();
+      });
+    };
+
+    loop();
+  },
+
+  render: function() {
+    let {mouseX, mouseY, springs} = this.state;
+    let box = {
+      width: 500,
+      height: 600,
+      backgroundColor: 'lightgray',
+    };
+    let s = {
+      position: 'absolute',
+      border: '1px solid black',
+      borderRadius: 99,
+      width: 50,
+      height: 50,
+      backgroundColor: 'red',
+    };
+
+    return (
+      <div onMouseMove={this.handleMouseMove} onMouseDown={this.handleMouseMove} style={box}>
+        {springs.map(([x, y], i) => {
+          return (
+            <div key={i} style={{
+              ...s,
+              WebkitTransform: `translate3d(${x}px, ${y}px, 0)`,
+              zIndex: springs.length - i,
+            }} />
+          );
+        })}
+      </div>
+    );
+  }
+});
+
+module.exports = App;
