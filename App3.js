61d5f91d39203123b37b4d8205ce63819a4cb39c
Cheng Lou <chenglou92@gmail.com>
2015-05-30 02:22:05 -0400

    Lol, layout spring

--- App3.js
+++ App3.js
@@ -82,26 +82,110 @@ var App = React.createClass({
   }
 });
 
-App = React.createClass({
-  getInitialState: function() {
-    return computeLayout({
-      style: {width: 500, height: 300, padding: 20, flexDirection: 'column'},
+// given a css-layout output tree, generate a homoiconic tree with values being
+// speed (for spring) rather than the x values
+function genVTree({width, height, top, left, children}) {
+  return {
+    width: 0,
+    height: 0,
+    top: 0,
+    left: 0,
+    children: children ? children.map(genVTree) : undefined,
+  };
+}
+
+// assuming a and b are homoiconic
+function map3Tree({width, height, top, left, children}, b, c, f) {
+  return {
+    width: f(width, b.width, c.width),
+    height: f(height, b.height, c.height),
+    top: f(top, b.top, c.top),
+    left: f(left, b.left, c.left),
+    children: children
+      ? children.map((child, i) => map3Tree(child, b.children[i], c.children[i], f))
+      : undefined,
+  }
+}
+
+let layout1 = {
+  style: {width: 500, height: 300, padding: 20, flexDirection: 'column'},
+  children: [
+    {
+      style: {justifyContent: 'space-between', flexDirection: 'row'},
       children: [
-        {
-          style: {justifyContent: 'space-between', flexDirection: 'row'},
-          children: [
-            {
-              style: {flex: 1}
-            },
-            {
-              style: {width: 100, height: 100}
-            },
-          ],
-        },
         {
           style: {flex: 1}
         },
+        {
+          style: {width: 100, height: 100}
+        },
       ],
+    },
+    {
+      style: {flex: 1}
+    },
+  ],
+};
+
+let layout2 = {
+  style: {width: 500, height: 500, padding: 20, flexDirection: 'column'},
+  children: [
+    {
+      style: {justifyContent: 'space-between', flexDirection: 'row'},
+      children: [
+        {
+          style: {flex: 1}
+        },
+        {
+          style: {width: 120, height: 120}
+        },
+      ],
+    },
+    {
+      style: {flex: 1}
+    },
+  ],
+};
+
+App = React.createClass({
+  getInitialState: function() {
+    let layout = computeLayout(layout1);
+
+    return {
+      // in-flight, animated. values computed with spring
+      layout: layout,
+      destLayout: layout,
+      v: genVTree(layout),
+      toggle: false,
+    };
+  },
+
+  componentDidMount: function() {
+    let loop = () => {
+      requestAnimationFrame(() => {
+        let {layout, v, destLayout} = this.state;
+        let newLayout =
+          map3Tree(layout, v, destLayout, (x, vx, destX) => stepper(x, vx, destX, 120, 16)[0]);
+        let newV =
+          map3Tree(layout, v, destLayout, (x, vx, destX) => stepper(x, vx, destX, 120, 16)[1]);
+
+        this.setState({
+          layout: newLayout,
+          v: newV,
+        })
+
+        loop();
+      });
+    };
+
+    loop();
+  },
+
+  handleClick: function() {
+    let toggle = this.state.toggle;
+    this.setState({
+      destLayout: toggle ? computeLayout(layout1) : computeLayout(layout2),
+      toggle: !toggle,
     });
   },
 
@@ -115,7 +199,7 @@ App = React.createClass({
         desc
       ],
       ...container,
-    } = this.state;
+    } = this.state.layout;
 
     let s = {
       outline: '1px solid black',
