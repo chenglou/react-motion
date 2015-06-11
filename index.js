8109744ea43ac1c847ff8dda3ea130576acdc922
Cheng Lou <chenglou92@gmail.com>
2015-06-10 23:58:13 -0400

    Working on 6

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 let React = require('react');
+let App0 = require('./App0');
 let App1 = require('./App1');
 let App2 = require('./App2');
 let App3 = require('./App3');
 let App4 = require('./App4');
+let App5 = require('./App5');
+let App6 = require('./App6');
 
+// React.render(<App0 />, document.querySelector('#content'));
 // React.render(<App1 />, document.querySelector('#content'));
 // React.render(<App2 />, document.querySelector('#content'));
 // React.render(<App3 />, document.querySelector('#content'));
-React.render(<App4 />, document.querySelector('#content'));
+// React.render(<App4 />, document.querySelector('#content'));
+// React.render(<App5 />, document.querySelector('#content'));
+React.render(<App6 />, document.querySelector('#content'));
