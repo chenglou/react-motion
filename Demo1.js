9b87d3e4d0d122424be8af1bcdac04f5219f6412
Cheng Lou <chenglou92@gmail.com>
2015-06-27 19:44:36 -0700

    Mostly working demo 1
    New feature: `update` function passed as arg to `finalVals`. Marks which
    subtree to update, and what spring physics params.
    
    Known bug: update, lol.

--- Demo1.js
+++ Demo1.js
@@ -4,6 +4,7 @@ import React from 'react';
 import Springs from './Springs';
 import {range, clone} from './utils';
 
+// indexed by visual position
 let layout = range(11).map(n => {
   let row = Math.floor(n / 3);
   let col = n % 3;
@@ -23,7 +24,7 @@ export default React.createClass({
     return {
       mouseX: 0,
       mouseY: 0,
-      press: null,
+      pressedComp: null,
       // index: visual position. value: component key/id
       order: range(11),
     };
@@ -31,12 +32,12 @@ export default React.createClass({
 
   handleMouseMove: function(e) {
     let {pageX, pageY} = e;
-    let {order, press} = this.state;
-    if (press != null) {
+    let {order, pressedComp} = this.state;
+    if (pressedComp != null) {
       let col = Math.min(Math.floor(pageX / 70), 2);
       let row = Math.min(Math.floor(pageY / 90), Math.floor(Object.keys(order).length / 3));
       let index = row * 3 + col;
-      let newOrder = reinsert(order, order.indexOf(press), index);
+      let newOrder = reinsert(order, order.indexOf(pressedComp), index);
       this.setState({mouseX: pageX, mouseY: pageY, order: newOrder});
     } else {
       this.setState({mouseX: pageX, mouseY: pageY});
@@ -44,15 +45,15 @@ export default React.createClass({
   },
 
   handleMouseDown: function(key) {
-    this.setState({press: key});
+    this.setState({pressedComp: key});
   },
 
   handleMouseUp: function() {
-    this.setState({press: null});
+    this.setState({pressedComp: null});
   },
 
   render: function() {
-    let {mouseX, mouseY, order, press} = this.state;
+    let {mouseX, mouseY, order, pressedComp} = this.state;
     let box = {
       width: 500,
       height: 600,
@@ -69,21 +70,38 @@ export default React.createClass({
 
     return (
       <div onMouseMove={this.handleMouseMove} onMouseUp={this.handleMouseUp} style={box}>
-        <Springs finalVals={() => order.map((_, i) => layout[order.indexOf(i)])}>
-          {
-            currVals => currVals.map(([x, y], i) => {
-              return (
-                <div
-                  key={i}
-                  onMouseDown={this.handleMouseDown.bind(null, i)}
-                  style={{
-                    ...s,
-                    WebkitTransform: `translate3d(${x}px, ${y}px, 0)`,
-                    transform: `translate3d(${x}px, ${y}px, 0)`,
-                  }
-                }>{i}</div>
-              );
-            })
+        <Springs finalVals={(_, update) => {
+          return {
+            order: update(order.map((_, key) => {
+              if (key === pressedComp) {
+                // nested (children) update takes priority. k=0, b=1 makes
+                // spring infinitely rigid
+                return update([mouseX, mouseY], 0, 1);
+              }
+              let visualPosition = order.indexOf(key);
+              return layout[visualPosition];
+            })),
+            scale: update(range(11).map((_, key) => pressedComp === key ? 1.2 : 1), 180, 10),
+          };
+        }}>
+          {data => {
+            return data.order.map(([x, y], key) => {
+              if (key === 4) {
+                // console.log(x, y, data.scale[key]);
+              }
+            return (
+              <div
+                key={key}
+                onMouseDown={this.handleMouseDown.bind(null, key)}
+                style={{
+                  ...s,
+                  WebkitTransform: `translate3d(${x}px, ${y}px, 0) scale(${data.scale[key]})`,
+                  transform: `translate3d(${x}px, ${y}px, 0) scale(${data.scale[key]})`,
+                  zIndex: key === pressedComp ? 99 : 1,
+                }
+              }>{key}</div>
+            );
+          });}
           }
         </Springs>
       </div>
--- Springs.jsx
+++ Springs.jsx
@@ -7,10 +7,108 @@ function zero() {
   return 0;
 }
 
+// dv = defaultValue
+// iv = initialValue, used each frame, power user modification to curr vals
+// fv = finalValue
+
+// dv={obj}
+// iv={cur => obj}
+// fv={cur => obj}
+// 1. no anim
+// 2. has anim
+// a. dv, iv, fv
+// b. iv, fv
+
+// 1a.
+// 1b.
+
+// iv={cur? => obj}
+// fv={cur? => obj}
+
+// see stepper for constant k, b usage
+function update(tree, k = 120, b = 16) {
+  return {
+    __springK: k,
+    __springB: b,
+    value: tree,
+  };
+}
+
+function stripMarks(tree) {
+  if (tree != null && tree.__springK != null) {
+    return stripMarks(tree.value);
+  }
+  if (Object.prototype.toString.call(tree) === '[object Array]') {
+    return tree.map(stripMarks);
+  }
+  if (Object.prototype.toString.call(tree) === '[object Object]') {
+    let newTree = {};
+    Object.keys(tree).forEach(key => newTree[key] = stripMarks(tree[key]));
+    return newTree;
+  }
+  // scalar
+  return tree;
+}
+
+function stripNothingButMarks(tree) {
+  if (tree != null && tree.__spring) {
+    return stripMarks(tree.value);
+  }
+  if (Object.prototype.toString.call(tree) === '[object Array]') {
+    return tree.map(stripMarks);
+  }
+  if (Object.prototype.toString.call(tree) === '[object Object]') {
+    let newTree = {};
+    Object.keys(tree).forEach(key => newTree[key] = stripMarks(tree[key]));
+    return newTree;
+  }
+  // scalar
+  return tree;
+}
+
+// see stepper for constant k, b usage
+function updateVals(currVals, currV, destVals, k, b) {
+  if (destVals != null && destVals.__springK != null) {
+    return updateVals(currVals, currV, destVals.value, destVals.__springK, destVals.__springB);
+  }
+  if (Object.prototype.toString.call(destVals) === '[object Array]') {
+    return destVals.map((val, i) => updateVals(currVals[i], currV[i], val, k, b));
+  }
+  if (Object.prototype.toString.call(destVals) === '[object Object]') {
+    let newTree = {};
+    Object.keys(destVals).forEach(key => {
+      newTree[key] = updateVals(currVals[key], currV[key], destVals[key], k, b);
+    });
+    return newTree;
+  }
+  // scalar
+  return stepper(currVals, currV, destVals, k, b)[0];
+}
+
+function updateV(currVals, currV, destVals, k, b) {
+  if (destVals != null && destVals.__springK != null) {
+    return updateV(currVals, currV, destVals.value, destVals.__springK, destVals.__springB);
+  }
+  if (Object.prototype.toString.call(destVals) === '[object Array]') {
+    return destVals.map((val, i) => updateV(currVals[i], currV[i], val, k, b));
+  }
+  if (Object.prototype.toString.call(destVals) === '[object Object]') {
+    let newTree = {};
+    Object.keys(destVals).forEach(key => {
+      newTree[key] = updateV(currVals[key], currV[key], destVals[key], k, b);
+    });
+    return newTree;
+  }
+  // scalar
+  return stepper(currVals, currV, destVals, k, b)[1];
+}
+
 export default React.createClass({
   getInitialState: function() {
     let {startVal, finalVals} = this.props;
-    let defaultVals = startVal || finalVals(null);
+    let defaultVals = stripMarks(
+      (startVal && startVal(null, update)) || finalVals(null, update)
+    );
     return {
       currVals: defaultVals,
       currV: mapTree(zero, defaultVals),
@@ -22,38 +120,47 @@ export default React.createClass({
       let {currVals, currV} = this.state;
       let {finalVals, defaultNewTreeVal} = this.props;
 
-      let destVals = finalVals(currVals);
+      // marked up update()s
+      // use this exact tree with its annotation
+      // interpolate
+      // return mark-less current values tree
+      let markedDestVals = finalVals(currVals, update);
 
-      let patchedCurrV = reshapeTree(
-        destVals,
-        currV,
-        // TODO: expose
-        (_, val) => mapTree(zero, val),
-      );
-      let patchedCurrVals = reshapeTree(
-        destVals,
-        currVals,
-        // TODO: le expose
-        defaultNewTreeVal || ((_, val) => mapTree(zero, val)),
-      );
+      let newCurrVals = updateVals(currVals, currV, markedDestVals, 120, 16);
+      let newCurrV = updateV(currVals, currV, markedDestVals, 120, 16);
 
-      patchedCurrVals = currVals;
-      patchedCurrV = currV;
 
-      let newCurrVals = mapTree(
-        // TODO: expose spring params
-        (_, x, vx, destX) => stepper(x, vx, destX, 120, 16)[0],
-        patchedCurrVals,
-        patchedCurrV,
-        destVals,
-      );
-      let newCurrV = mapTree(
-        // TODO: expose spring params
-        (_, x, vx, destX) => stepper(x, vx, destX, 120, 16)[1],
-        patchedCurrVals,
-        patchedCurrV,
-        destVals,
-      );
+
+      // let patchedCurrV = reshapeTree(
+      //   destVals,
+      //   currV,
+      //   // TODO: expose
+      //   (_, val) => mapTree(zero, val),
+      // );
+      // let patchedCurrVals = reshapeTree(
+      //   destVals,
+      //   currVals,
+      //   // TODO: le expose
+      //   defaultNewTreeVal || ((_, val) => mapTree(zero, val)),
+      // );
+
+      // patchedCurrVals = currVals;
+      // patchedCurrV = currV;
+
+      // let newCurrVals = mapTree(
+      //   // TODO: expose spring params
+      //   (_, x, vx, destX) => stepper(x, vx, destX, 120, 16)[0],
+      //   patchedCurrVals,
+      //   patchedCurrV,
+      //   destVals,
+      // );
+      // let newCurrV = mapTree(
+      //   // TODO: expose spring params
+      //   (_, x, vx, destX) => stepper(x, vx, destX, 120, 16)[1],
+      //   patchedCurrVals,
+      //   patchedCurrV,
+      //   destVals,
+      // );
 
       this.setState(() => {
         return {
