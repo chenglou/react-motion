51bb5da277e843ca23ea62e25e514a5f14c11974
Cheng Lou <chenglou92@gmail.com>
2015-06-29 02:03:10 -0700

    Well this just obliterated anything else
    Here's an informative commit msg. Oh btw iamdustan: if you're reading this, try demo2 lol.

--- index.jsx
+++ index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Demo1 from './Demo1';
-import Demo3 from './Demo3';
+import Demo2 from './Demo2';
 
 // React.render(<Demo1 />, document.querySelector('#content'));
-React.render(<Demo3 />, document.querySelector('#content'));
+React.render(<Demo2 />, document.querySelector('#content'));
