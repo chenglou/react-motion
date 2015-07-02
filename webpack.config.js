49610202f2787d28d46670f83e71d087d4e3ab8e
Dan Abramov <dan.abramov@gmail.com>
2015-07-03 01:07:13 +0200

    Add prod build mode

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,59 @@
 var webpack = require('webpack');
 
-function wrapEntry(entry) {
-  return [
-    entry,
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server'
-  ];
+var devtool;
+var loaders = ['babel?stage=0'];
+var plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
+];
+var entry = {
+  demo1: './demo1/index.jsx',
+  demo2: './demo2/index.jsx',
+  demo3: './demo3/index.jsx',
+  demo4: './demo4/index.jsx'
+};
+
+var DEV = process.env.NODE_ENV !== 'production';
+if (DEV) {
+  devtool = 'eval';
+  loaders = ['react-hot'].concat(loaders);
+  plugins = plugins.concat([
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ]);
+  entry = Object.keys(entry).reduce(function (result, key) {
+    result[key] = [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      entry[key]
+    ];
+    return result;
+  }, {});
+} else {
+  plugins = plugins.concat([
+    new webpack.optimize.OccurenceOrderPlugin()
+  ]);
 }
 
 module.exports = {
-  entry: {
-    demo1: wrapEntry('./demo1/index.jsx'),
-    demo2: wrapEntry('./demo2/index.jsx'),
-    demo3: wrapEntry('./demo3/index.jsx'),
-    demo4: wrapEntry('./demo4/index.jsx'),
-  },
+  devtool: devtool,
+  entry: entry,
   output: {
     filename: './[name]/all.js',
     publicPath: '/',
-    path: '/'
+    path: __dirname
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /build|node_modules/,
-      loaders: ['react-hot', 'babel?stage=0']
+      loaders: loaders
     }]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+  plugins: plugins
 };
 
