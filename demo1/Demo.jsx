e37400bb2928a16fad0993d4d136f9c2068a5ffb
Cheng Lou <chenglou92@gmail.com>
2015-07-05 02:47:03 +0200

    Make demo1 variable names clearer

--- demo1/Demo.jsx
+++ demo1/Demo.jsx
@@ -15,13 +15,13 @@ let Demo = React.createClass({
     this.handleMouseMove(touches[0]);
   },
 
-  getValues(update, positions) {
-    // positions of `null` means it's the first render for Spring
-    if (positions == null) {
+  getValues(update, currentPositions) {
+    // currentPositions of `null` means it's the first render for Spring
+    if (currentPositions == null) {
       return range(6).map(() => [0, 0]);
     }
-    let endValue = positions.reduce((acc, _, i) => {
-      return i === 0 ? [this.state.mouse] : [...acc, positions[i - 1]];
+    let endValue = currentPositions.reduce((acc, _, i) => {
+      return i === 0 ? [this.state.mouse] : [...acc, currentPositions[i - 1]];
     }, []);
     // `update` is a function passed to you for tweaking your collection's spring
     // constants. 120 is the stiffness, 17 is the damping. This will update every
@@ -36,14 +36,14 @@ let Demo = React.createClass({
         endValue={this.getValues}
         onMouseMove={this.handleMouseMove}
         onTouchMove={this.handleTouchMove}>
-        {positions => positions.map(([x, y], i) =>
+        {currentPositions => currentPositions.map(([x, y], i) =>
           <div
             key={i}
             className={`demo1-ball ball-${i}`}
             style={{
               WebkitTransform: `translate3d(${x - 25}px, ${y - 25}px, 0)`,
               transform: `translate3d(${x - 25}px, ${y - 25}px, 0)`,
-              zIndex: positions.length - i,
+              zIndex: currentPositions.length - i,
             }} />
         )}
       </Spring>
