b20df9778b730b155b89eeeb976169942c3daee4
Cheng Lou <chenglou92@gmail.com>
2015-07-06 23:27:54 -0700

    Turn demo0 into new format

--- demo0/Demo.jsx
+++ demo0/Demo.jsx
@@ -14,13 +14,13 @@ let Demo = React.createClass({
     return (
       <div>
         <button onMouseDown={this.handleMouseDown}>Toggle</button>
-        <Spring className="demo0" endValue={this.state.open ? 400 : 0}>
-          {x =>
+        <Spring className="demo0" endValue={{val: this.state.open ? 400 : 0}}>
+          {({val}) =>
             // children is a callback which should accept the current value of
             // `endValue`
             <div className="demo0-block" style={{
-              WebkitTransform: `translate3d(${x}px, 0, 0)`,
-              transform: `translate3d(${x}px, 0, 0)`,
+              WebkitTransform: `translate3d(${val}px, 0, 0)`,
+              transform: `translate3d(${val}px, 0, 0)`,
             }} />
           }
         </Spring>
