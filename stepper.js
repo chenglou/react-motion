a12a03b2aac380b4281e94b4a86e98e31d32461e
Nik Butenko <nik@butenko.me>
2015-07-10 09:49:56 +1000

    Syntax: use const

--- stepper.js
+++ stepper.js
@@ -1,22 +1,22 @@
-let errorMargin = 0.0001;
+const errorMargin = 0.0001;
 
 export default function stepper(frameRate, x, v, destX, k, b) {
   // Spring stiffness, in kg / s^2
 
   // for animations, destX is really spring length (spring at rest). initial
   // position is considered as the stretched/compressed position of a spring
-  let Fspring = -k * (x - destX);
+  const Fspring = -k * (x - destX);
 
   // Damping constant, in kg / s
-  let Fdamper = -b * v;
+  const Fdamper = -b * v;
 
   // usually we put mass here, but for animation purposes, specifying mass is a
   // bit redundant. you could simply adjust k and b accordingly
   // let a = (Fspring + Fdamper) / mass;
-  let a = Fspring + Fdamper;
+  const a = Fspring + Fdamper;
 
-  let newX = x + v * frameRate;
-  let newV = v + a * frameRate;
+  const newX = x + v * frameRate;
+  const newV = v + a * frameRate;
 
   if (Math.abs(newV - v) < errorMargin && Math.abs(newX - x) < errorMargin) {
     return [destX, 0];
