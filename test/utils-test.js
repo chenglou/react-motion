666d8556d5a417b38fbc5f71988437b5c023dc23
Cheng Lou <chenglou92@gmail.com>
2015-07-13 00:45:46 -0700

    Add tests for mapTree

---
+++ test/utils-test.js
@@ -0,0 +1,36 @@
+import expect from 'expect';
+import * as u from '../utils';
+import React from 'react';
+
+describe('mapTree', () => {
+  it('should map leaves correctly', () => {
+    const add1 = (_, a) => a + 1;
+    expect(u.mapTree(add1, 1)).toEqual(2);
+    expect(u.mapTree(add1, 'hi')).toEqual('hi1'); // lol js
+    expect(u.mapTree(add1, [])).toEqual([]);
+    expect(u.mapTree(add1, {})).toEqual({});
+    expect(u.mapTree(add1, {a: 1})).toEqual({a: 2});
+    expect(u.mapTree(add1, {a: [1, {b: 2}]})).toEqual({a: [2, {b: 3}]});
+  });
+
+  it('should be not infinitely recurse into structures we provide ', () => {
+    expect(u.mapTree(() => [1, 2], {a: [1, {b: 2}]}))
+      .toEqual({a: [[1, 2], {b: [1, 2]}]});
+  });
+
+  it('should not call f on non-ground types', () => {
+    let add1 = {
+      f: () => {
+        throw new Error('asd');
+      },
+    };
+    const now = new Date();
+    const MyClass = class {};
+    const inst = new MyClass();
+    const comp = <div key="a" />;
+    expect(u.mapTree(add1.f, now)).toEqual(now);
+    expect(u.mapTree(add1.f, MyClass)).toEqual(MyClass);
+    expect(u.mapTree(add1.f, inst)).toEqual(inst);
+    expect(u.mapTree(add1.f, comp)).toEqual(comp);
+  });
+});
--- utils.js
+++ utils.js
@@ -1,10 +1,98 @@
+function isPlainObject(obj) {
+  return obj ? typeof obj === 'object' &&
+    Object.getPrototypeOf(obj) === Object.prototype : false;
+}
+
 // damn it JS
 export function clone(coll) {
   return JSON.parse(JSON.stringify(coll));
 }
 
-export function eq(a, b) {
-  return JSON.stringify(a) === JSON.stringify(b);
+// export function eq(a, b) {
+//   return JSON.stringify(a) === JSON.stringify(b);
+// }
+
+// currenly a helper used for producing a tree of the same shape as the
+// input(s),  but with different values. It's technically not a real `map`
+// equivalent for trees, since it skips calling f on non-scalars.
+
+// TODO: probably doesn't need path, stop allocating uselessly
+// TODO: don't need to map over many trees anymore
+// TODO: skipping non-scalars is weird and non-generic. Use pre-order traversal
+// assume trees are of the same shape
+function _mapTree(path, f, trees) {
+  const t1 = trees[0];
+  if (typeof t1 === 'number' || typeof t1 === 'string') {
+    return f(path, ...trees);
+  }
+  if (Array.isArray(t1)) {
+    return t1.map((_, i) => _mapTree([...path, i], f, trees.map(val => val[i])));
+  }
+  if (isPlainObject(t1)) {
+    const newTree = {};
+    Object.keys(t1).forEach(key => {
+      newTree[key] = _mapTree([...path, key], f, trees.map(val => val[key]));
+    });
+    return newTree;
+  }
+  // return last one just because
+  return trees[trees.length - 1];
+}
+
+export function mapTree(f, ...rest) {
+  return _mapTree([], f, rest);
+}
+
+// function _reshapeTree(path, a, b, f) {
+//   if (a == null) {
+//     throw new Error('wtf2');
+//   }
+
+//   if (b == null) {
+//     return f(path, a);
+//   }
+
+//   if (Array.isArray(a)) {
+//     return a.map((val, i) => _reshapeTree([...path, i], val, b[i], f));
+//   }
+//   if (Object.prototype.toString.call(a) === '[object Object]') {
+//     const newTree = {};
+//     Object.keys(a).forEach(key => {
+//       newTree[key] = _reshapeTree([...path, key], a[key], b[key], f);
+//     });
+//     return newTree;
+//   }
+
+//   return b;
+// }
+
+// export function reshapeTree(a, b, f) {
+//   return _reshapeTree([], a, b, f);
+// }
+
+// export function toOj(vals, keys) {
+//   const ret = {};
+//   vals.forEach((val, i) => ret[keys[i]] = val);
+//   return ret;
+// }
+
+// export function toArr(obj) {
+//   const keys = Object.keys(obj);
+//   const vals = keys.map(k => obj[k]);
+//   return [keys, vals];
+// }
+
+// TODO: these are for a demos, not for the library. Move
+export function reinsert(arr, from, to) {
+  const _arr = arr.slice(0);
+  const val = _arr[from];
+  _arr.splice(from, 1);
+  _arr.splice(to, 0, val);
+  return _arr;
+}
+
+export function clamp(n, min, max) {
+  return Math.max(Math.min(n, max), min);
 }
 
 export function range(start, afterStop) {
@@ -19,75 +107,4 @@ export function range(start, afterStop) {
     ret.push(i);
   }
   return ret;
-}
-
-// assume trees same are same
-function _mapTree(path, f, trees) {
-  const t1 = trees[0];
-  if (Array.isArray(t1)) {
-    return t1.map((_, i) => _mapTree([...path, i], f, trees.map(val => val[i])));
-  }
-  if (Object.prototype.toString.call(t1) === '[object Object]') {
-    const newTree = {};
-    Object.keys(t1).forEach(key => {
-      newTree[key] = _mapTree([...path, key], f, trees.map(val => val[key]));
-    });
-    return newTree;
-  }
-  return f(path, ...trees);
-}
-
-export function mapTree(f, ...rest) {
-  return _mapTree([], f, rest);
-}
-
-function _reshapeTree(path, a, b, f) {
-  if (a == null) {
-    throw new Error('wtf2');
-  }
-
-  if (b == null) {
-    return f(path, a);
-  }
-
-  if (Array.isArray(a)) {
-    return a.map((val, i) => _reshapeTree([...path, i], val, b[i], f));
-  }
-  if (Object.prototype.toString.call(a) === '[object Object]') {
-    const newTree = {};
-    Object.keys(a).forEach(key => {
-      newTree[key] = _reshapeTree([...path, key], a[key], b[key], f);
-    });
-    return newTree;
-  }
-
-  return b;
-}
-
-export function reshapeTree(a, b, f) {
-  return _reshapeTree([], a, b, f);
-}
-
-export function toOj(vals, keys) {
-  const ret = {};
-  vals.forEach((val, i) => ret[keys[i]] = val);
-  return ret;
-}
-
-export function toArr(obj) {
-  const keys = Object.keys(obj);
-  const vals = keys.map(k => obj[k]);
-  return [keys, vals];
-}
-
-export function reinsert(arr, from, to) {
-  const _arr = arr.slice(0);
-  const val = _arr[from];
-  _arr.splice(from, 1);
-  _arr.splice(to, 0, val);
-  return _arr;
-}
-
-export function clamp(n, min, max) {
-  return Math.max(Math.min(n, max), min);
 }
