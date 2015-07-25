69353d50c489f83160b36e054cb08b5a0b4eb315
Cheng Lou <chenglou92@gmail.com>
2015-07-25 01:02:03 -0700

    Revert "Clean up animationLoop; Extract logic from both Springs"
    This reverts commit a9ce2bd5367f4f9786265820bb1246c3a691ff7e.

--- src/Spring.js
+++ src/Spring.js
@@ -4,9 +4,16 @@ import isPlainObject from 'lodash.isplainobject';
 import stepper from './stepper';
 import noVelocity from './noVelocity';
 import mergeDiff from './mergeDiff';
-import configAnimation from './animationLoop';
+import createAnimationLoop from './animationLoop';
 
-const startAnimation = configAnimation();
+const animationLoop = createAnimationLoop({
+  // Fixed time step in seconds.
+  timeStep: 1 / 60,
+  // Slow-mo anyone? Give 0.1 a try.
+  timeScale: 1,
+  // Pause if we have more than this many steps worth of accumulated time.
+  maxSteps: 10,
+});
 
 function zero() {
   return 0;
@@ -121,50 +128,6 @@ export function updateCurrVelocity(frameRate, currValue, currVelocity, endValue,
   return mapTree(zero, currVelocity);
 }
 
-function animationStep(shouldMerge, stopAnimation, getProps, timestep, state) {
-  let {currValue, currVelocity} = state;
-  let {willEnter, willLeave, endValue} = getProps();
-
-  if (typeof endValue === 'function') {
-    endValue = endValue(currValue);
-  }
-
-  let mergedValue = endValue; // set mergedValue to endValue as the default
-  let hasNewKey = false;
-
-  if (shouldMerge) {
-    mergedValue = mergeDiff(
-      currValue,
-      endValue,
-      // TODO: stop allocating like crazy in this whole code path
-      key => willLeave(key, currValue[key], endValue, currValue, currVelocity)
-    );
-
-    Object.keys(mergedValue)
-      .filter(key => !currValue.hasOwnProperty(key))
-      .forEach(key => {
-        hasNewKey = true;
-        const enterValue = willEnter(key, mergedValue[key], endValue, currValue, currVelocity);
-        currValue[key] = enterValue;
-        mergedValue[key] = enterValue;
-        currVelocity[key] = mapTree(zero, currValue[key]);
-      });
-  }
-
-  const newCurrValue = updateCurrValue(timestep, currValue, currVelocity, mergedValue);
-  const newCurrVelocity = updateCurrVelocity(timestep, currValue, currVelocity, mergedValue);
-
-  if (!hasNewKey && noVelocity(currVelocity) && noVelocity(newCurrVelocity)) {
-    // check explanation in `Spring.animationRender`
-    stopAnimation(); // Nasty side effects....
-  }
-
-  return {
-    currValue: newCurrValue,
-    currVelocity: newCurrVelocity,
-  };
-}
-
 export const Spring = React.createClass({
   propTypes: {
     endValue: PropTypes.oneOfType([
@@ -188,7 +151,6 @@ export const Spring = React.createClass({
   },
 
   componentDidMount() {
-    this.animationStep = animationStep.bind(null, false, () => this.stopAnimation(), () => this.props);
     this.startAnimating();
   },
 
@@ -196,25 +158,54 @@ export const Spring = React.createClass({
     this.startAnimating();
   },
 
-  stopAnimation: null,
+  unsubscribeAnimation: null,
 
   // used in animationRender
   hasUnmounted: false,
 
-  animationStep: null,
-
   componentWillUnmount() {
-    this.stopAnimation();
+    if (this.unsubscribeAnimation) {
+      this.unsubscribeAnimation();
+      this.unsubscribeAnimation = null;
+    }
     this.hasUnmounted = true;
   },
 
   startAnimating() {
-    // Is smart enough to not start it twice
-    this.stopAnimation = startAnimation(
-      this.state,
-      this.animationStep,
-      this.animationRender,
-    );
+    if (!this.unsubscribeAnimation) {
+      // means we're not animating
+      this.unsubscribeAnimation = animationLoop.subscribe(
+        this.animationStep,
+        this.animationRender,
+        this.state,
+      );
+      animationLoop.start();
+    }
+  },
+
+  animationStep(timeStep, state) {
+    const {currValue, currVelocity} = state;
+    let {endValue} = this.props;
+
+    if (typeof endValue === 'function') {
+      endValue = endValue(currValue);
+    }
+
+    const newCurrValue = updateCurrValue(timeStep, currValue, currVelocity, endValue);
+    const newCurrVelocity = updateCurrVelocity(timeStep, currValue, currVelocity, endValue);
+
+    if (noVelocity(currVelocity) && noVelocity(newCurrVelocity)) {
+      // check explanation in `animationRender`
+      if (!this.hasUnmounted) {
+        this.unsubscribeAnimation();
+        this.unsubscribeAnimation = null;
+      }
+    }
+
+    return {
+      currValue: newCurrValue,
+      currVelocity: newCurrVelocity,
+    };
   },
 
   animationRender(alpha, nextState, prevState) {
@@ -282,7 +273,6 @@ export const TransitionSpring = React.createClass({
   },
 
   componentDidMount() {
-    this.animationStep = animationStep.bind(null, true, () => this.stopAnimation(), () => this.props);
     this.startAnimating();
   },
 
@@ -290,23 +280,73 @@ export const TransitionSpring = React.createClass({
     this.startAnimating();
   },
 
-  stopAnimation: null,
+  unsubscribeAnimation: null,
 
   // used in animationRender
   hasUnmounted: false,
 
-  animationStep: null,
-
   componentWillUnmount() {
-    this.stopAnimation();
+    if (this.unsubscribeAnimation) {
+      this.unsubscribeAnimation();
+      this.unsubscribeAnimation = undefined;
+    }
   },
 
   startAnimating() {
-    this.stopAnimation = startAnimation(
-      this.state,
-      this.animationStep,
-      this.animationRender,
+    if (!this.unsubscribeAnimation) {
+      this.unsubscribeAnimation = animationLoop.subscribe(
+        this.animationStep,
+        this.animationRender,
+        this.state
+      );
+      animationLoop.start();
+    }
+  },
+
+  animationStep(timeStep, state) {
+    let {currValue, currVelocity} = state;
+    let {endValue} = this.props;
+    const {willEnter, willLeave} = this.props;
+
+    if (typeof endValue === 'function') {
+      endValue = endValue(currValue);
+    }
+
+    let mergedValue;
+    // only other option is obj
+    mergedValue = mergeDiff(
+      currValue,
+      endValue,
+      // TODO: stop allocating like crazy in this whole code path
+      key => willLeave(key, currValue[key], endValue, currValue, currVelocity)
     );
+
+    let hasNewKey = false;
+    Object.keys(mergedValue)
+      .filter(key => !currValue.hasOwnProperty(key))
+      .forEach(key => {
+        hasNewKey = true;
+        const enterValue = willEnter(key, mergedValue[key], endValue, currValue, currVelocity);
+        currValue[key] = enterValue;
+        mergedValue[key] = enterValue;
+        currVelocity[key] = mapTree(zero, currValue[key]);
+      });
+
+    const newCurrValue = updateCurrValue(timeStep, currValue, currVelocity, mergedValue);
+    const newCurrVelocity = updateCurrVelocity(timeStep, currValue, currVelocity, mergedValue);
+
+    if (noVelocity(currVelocity) && noVelocity(newCurrVelocity) && !hasNewKey) {
+      // check explanation in `Spring.animationRender`
+      if (!this.hasUnmounted) {
+        this.unsubscribeAnimation();
+        this.unsubscribeAnimation = undefined;
+      }
+    }
+
+    return {
+      currValue: newCurrValue,
+      currVelocity: newCurrVelocity,
+    };
   },
 
   animationRender(alpha, nextState, prevState) {
@@ -321,7 +361,6 @@ export const TransitionSpring = React.createClass({
 
   render() {
     const {currValue} = this.state;
-
     return React.Children.only(this.props.children(currValue));
   },
 });
