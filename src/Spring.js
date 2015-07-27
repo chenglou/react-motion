098f3b2ebea151a05fb31e5b5225f0cd9452f58f
Cheng Lou <chenglou92@gmail.com>
2015-07-27 03:05:20 -0700

    Pull out interpolateValue, updateCurr*
    These have similar logic and are better off grouped together for future
    refactoring. @bsansouci

--- src/Spring.js
+++ src/Spring.js
@@ -1,122 +1,12 @@
 import React, {PropTypes} from 'react';
 import mapTree from './mapTree';
-import isPlainObject from 'lodash.isplainobject';
-import stepper from './stepper';
 import noVelocity from './noVelocity';
 import mergeDiff from './mergeDiff';
 import configAnimation from './animationLoop';
 import zero from './zero';
+import {interpolateValue, updateCurrValue, updateCurrVelocity} from './updateTree';
 
 const startAnimation = configAnimation();
-
-// TODO: refactor common logic with updateCurrValue and updateCurrVelocity
-function interpolateValue(alpha, nextValue, prevValue) {
-  if (nextValue == null) {
-    return null;
-  }
-  if (prevValue == null) {
-    return nextValue;
-  }
-  if (typeof nextValue === 'number') {
-    // https://github.com/chenglou/react-motion/pull/57#issuecomment-121924628
-    return nextValue * alpha + prevValue * (1 - alpha);
-  }
-  if (nextValue.val != null && nextValue.config && nextValue.config.length === 0) {
-    return nextValue;
-  }
-  if (nextValue.val != null) {
-    let ret = {
-      val: interpolateValue(alpha, nextValue.val, prevValue.val),
-    };
-    if (nextValue.config) {
-      ret.config = nextValue.config;
-    }
-    return ret;
-  }
-  if (Array.isArray(nextValue)) {
-    return nextValue.map((_, i) => interpolateValue(alpha, nextValue[i], prevValue[i]));
-  }
-  if (isPlainObject(nextValue)) {
-    return Object.keys(nextValue).reduce((ret, key) => {
-      ret[key] = interpolateValue(alpha, nextValue[key], prevValue[key]);
-      return ret;
-    }, {});
-  }
-  return nextValue;
-}
-
-// TODO: refactor common logic with updateCurrVelocity
-export function updateCurrValue(frameRate, currValue, currVelocity, endValue, k, b) {
-  if (endValue == null) {
-    return null;
-  }
-  if (typeof endValue === 'number') {
-    if (k == null || b == null) {
-      return endValue;
-    }
-    // TODO: do something to stepper to make this not allocate (2 steppers?)
-    return stepper(frameRate, currValue, currVelocity, endValue, k, b)[0];
-  }
-  if (endValue.val != null && endValue.config && endValue.config.length === 0) {
-    return endValue;
-  }
-  if (endValue.val != null) {
-    const [_k, _b] = endValue.config || [170, 26];
-    let ret = {
-      val: updateCurrValue(frameRate, currValue.val, currVelocity.val, endValue.val, _k, _b),
-    };
-    if (endValue.config) {
-      ret.config = endValue.config;
-    }
-    return ret;
-  }
-  if (Array.isArray(endValue)) {
-    return endValue.map((_, i) => updateCurrValue(frameRate, currValue[i], currVelocity[i], endValue[i], k, b));
-  }
-  if (isPlainObject(endValue)) {
-    return Object.keys(endValue).reduce((ret, key) => {
-      ret[key] = updateCurrValue(frameRate, currValue[key], currVelocity[key], endValue[key], k, b);
-      return ret;
-    }, {});
-  }
-  return endValue;
-}
-
-export function updateCurrVelocity(frameRate, currValue, currVelocity, endValue, k, b) {
-  if (endValue == null) {
-    return null;
-  }
-  if (typeof endValue === 'number') {
-    if (k == null || b == null) {
-      return mapTree(zero, currVelocity);
-    }
-    // TODO: do something to stepper to make this not allocate (2 steppers?)
-    return stepper(frameRate, currValue, currVelocity, endValue, k, b)[1];
-  }
-  if (endValue.val != null && endValue.config && endValue.config.length === 0) {
-    return mapTree(zero, currVelocity);
-  }
-  if (endValue.val != null) {
-    const [_k, _b] = endValue.config || [170, 26];
-    let ret = {
-      val: updateCurrVelocity(frameRate, currValue.val, currVelocity.val, endValue.val, _k, _b),
-    };
-    if (endValue.config) {
-      ret.config = endValue.config;
-    }
-    return ret;
-  }
-  if (Array.isArray(endValue)) {
-    return endValue.map((_, i) => updateCurrVelocity(frameRate, currValue[i], currVelocity[i], endValue[i], k, b));
-  }
-  if (isPlainObject(endValue)) {
-    return Object.keys(endValue).reduce((ret, key) => {
-      ret[key] = updateCurrVelocity(frameRate, currValue[key], currVelocity[key], endValue[key], k, b);
-      return ret;
-    }, {});
-  }
-  return mapTree(zero, currVelocity);
-}
 
 function animationStep(shouldMerge, stopAnimation, getProps, timestep, state) {
   let {currValue, currVelocity} = state;
---
+++ test/updateTree-test.js
@@ -0,0 +1,118 @@
+import React from 'react';
+import {updateCurrValue, updateCurrVelocity} from '../src/updateTree';
+
+const FRAME_RATE = 1 / 60;
+
+describe('updateCurrValue', () => {
+  it('should not error on null', () => {
+    expect(updateCurrValue(FRAME_RATE, {val: null}, {val: null}, {val: null}))
+      .toEqual({val: null});
+  });
+
+  it('should jump to endValue when there is no val wrapper correctly', () => {
+    const currValue = {count: 0};
+    const currVelocity = {count: 1};
+    const endValue = {count: 100};
+    expect(updateCurrValue(FRAME_RATE, currValue, currVelocity, endValue))
+      .toEqual({count: 100});
+  });
+
+  it('should jump to endValue when config is []', () => {
+    const currValue = {count: {val: 1, config: []}};
+    const currVelocity = {count: {val: 5, config: []}};
+    const endValue = {count: {val: 10, config: []}};
+    expect(updateCurrValue(FRAME_RATE, currValue, currVelocity, endValue))
+      .toEqual({count: {val: 10, config: []}});
+  });
+
+  it('should do top-level updates', () => {
+    const currValue = {val: [1, 2, 3]};
+    const currVelocity = {val: [5, 5, 5]};
+    const endValue = {val: [10, 10, 10]};
+    expect(updateCurrValue(FRAME_RATE, currValue, currVelocity, endValue)).toEqual({
+      val: [1.4722222222222223, 2.425, 3.3777777777777778],
+    });
+  });
+
+  it('should do negative numbers', () => {
+    const currValue = {val: 1};
+    const currVelocity = {val: 100};
+    const endValue = {val: -1000};
+    expect(updateCurrValue(FRAME_RATE, currValue, currVelocity, endValue)).toEqual({
+      val: -45.325,
+    });
+  });
+
+  it('should do nested updates, with a default config', () => {
+    const currValue = {count: {val: [1, 2, {a: 3}]}};
+    const currVelocity = {count: {val: [10, 20, {a: 30}]}};
+    const endValue = {count: {val: [100, 200, {a: 300}]}};
+    expect(updateCurrValue(FRAME_RATE, currValue, currVelocity, endValue)).toEqual({
+      count: {
+        val: [5.769444444444445, 11.53888888888889, {a: 17.308333333333334}],
+      },
+    });
+  });
+
+  it('should have nested val override upper ones', () => {
+    let currValue = {val: [2, {val: 2, config: [100, 10]}]};
+    let currVelocity = {val: [10, {val: 10, config: [100, 10]}]};
+    let endValue = {val: [2, {val: 2, config: [100, 10]}]};
+    expect(updateCurrValue(FRAME_RATE, currValue, currVelocity, endValue)).toEqual({
+      val: [2.0944444444444446, {val: 2.138888888888889, config: [100, 10]}],
+    });
+
+    currValue = {val: [1, {val: 1, config: []}]};
+    currVelocity = {val: [5, {val: 5, config: []}]};
+    endValue = {val: [10, {val: 10, config: []}]};
+    expect(updateCurrValue(FRAME_RATE, currValue, currVelocity, endValue)).toEqual({
+      val: [1.4722222222222223, {val: 10, config: []}],
+    });
+  });
+
+  it('should skip non-numerical values', () => {
+    const comp = <div key="1" />;
+    const currValue = {val: [2, 'hi', comp]};
+    const currVelocity = {val: [5, 'hi', comp]};
+    const endValue = {val: [10, 'hi', comp]};
+    expect(updateCurrValue(FRAME_RATE, currValue, currVelocity, endValue)).toEqual({
+      val: [2.425, 'hi', comp],
+    });
+  });
+});
+
+describe('updateCurrVelocity', () => {
+  it('should not error on null', () => {
+    expect(updateCurrVelocity(FRAME_RATE, {val: null}, {val: null}, {val: null}))
+      .toEqual({val: null});
+  });
+
+  // to potential contributors: these behaviors are not set in stone, but don't
+  // matter right now. It's debatable that we should e.g. in the below test keep
+  // currVelocity to {count: 0}
+  it('should have a velocity of 0 for non-updating values', () => {
+    const currValue = {count: 0};
+    const currVelocity = {count: 1};
+    const endValue = {count: 100};
+    expect(updateCurrVelocity(FRAME_RATE, currValue, currVelocity, endValue))
+      .toEqual({count: 0});
+  });
+
+  it('should have a velocity of 0 for config []', () => {
+    const currValue = {count: {val: 1, config: []}};
+    const currVelocity = {count: {val: 5, config: []}};
+    const endValue = {count: {val: 10, config: []}};
+    expect(updateCurrVelocity(FRAME_RATE, currValue, currVelocity, endValue))
+      .toEqual({count: {val: 0, config: []}});
+  });
+
+  it('should leave non-numerical values alone', () => {
+    const comp = <div key="1" />;
+    const currValue = {val: [1, ['hi'], comp]};
+    const currVelocity = {val: [1, ['hi'], comp]};
+    const endValue = {val: [10, ['hi'], comp]};
+    expect(updateCurrVelocity(FRAME_RATE, currValue, currVelocity, endValue)).toEqual({
+      val: [26.066666666666666, ['hi'], comp],
+    });
+  });
+});
