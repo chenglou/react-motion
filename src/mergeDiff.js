1a41b48a2375b3db06b0fcce571f48b1efad417b
Cheng Lou <chenglou92@gmail.com>
2015-07-27 23:31:00 -0700

    Small naming tweak to mergeDiff internal

--- src/mergeDiff.js
+++ src/mergeDiff.js
@@ -1,6 +1,6 @@
 // this function is allocation-less thanks to babel, which transforms the tail
 // calls into loops
-function mergeDiffArr2(arrA, arrB, collB, indexA, indexB, onRemove, accum) {
+function mergeDiffArr(arrA, arrB, collB, indexA, indexB, onRemove, accum) {
   const endA = indexA === arrA.length;
   const endB = indexB === arrB.length;
   const keyA = arrA[indexA];
@@ -13,7 +13,7 @@ function mergeDiffArr2(arrA, arrB, collB, indexA, indexB, onRemove, accum) {
 
   if (endA) {
     accum[keyB] = collB[keyB];
-    return mergeDiffArr2(arrA, arrB, collB, indexA, indexB + 1, onRemove, accum);
+    return mergeDiffArr(arrA, arrB, collB, indexA, indexB + 1, onRemove, accum);
   }
 
   if (endB) {
@@ -21,12 +21,12 @@ function mergeDiffArr2(arrA, arrB, collB, indexA, indexB, onRemove, accum) {
     if (fill != null) {
       accum[keyA] = fill;
     }
-    return mergeDiffArr2(arrA, arrB, collB, indexA + 1, indexB, onRemove, accum);
+    return mergeDiffArr(arrA, arrB, collB, indexA + 1, indexB, onRemove, accum);
   }
 
   if (keyA === keyB) {
     accum[keyA] = collB[keyA];
-    return mergeDiffArr2(arrA, arrB, collB, indexA + 1, indexB + 1, onRemove, accum);
+    return mergeDiffArr(arrA, arrB, collB, indexA + 1, indexB + 1, onRemove, accum);
   }
 
   if (!collB.hasOwnProperty(keyA)) {
@@ -34,16 +34,16 @@ function mergeDiffArr2(arrA, arrB, collB, indexA, indexB, onRemove, accum) {
     if (fill != null) {
       accum[keyA] = fill;
     }
-    return mergeDiffArr2(arrA, arrB, collB, indexA + 1, indexB, onRemove, accum);
+    return mergeDiffArr(arrA, arrB, collB, indexA + 1, indexB, onRemove, accum);
   }
 
-  return mergeDiffArr2(arrA, arrB, collB, indexA + 1, indexB, onRemove, accum);
+  return mergeDiffArr(arrA, arrB, collB, indexA + 1, indexB, onRemove, accum);
 }
 
 export default function mergeDiff(a, b, onRemove) {
   let ret = {};
   // if anyone can make this work without allocating the arrays here, we'll
   // give you a medal
-  mergeDiffArr2(Object.keys(a), Object.keys(b), b, 0, 0, onRemove, ret);
+  mergeDiffArr(Object.keys(a), Object.keys(b), b, 0, 0, onRemove, ret);
   return ret;
 }
