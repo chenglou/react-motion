91ffa055e4b0c85507b41a38d12cecd39941e295
Cheng Lou <chenglou92@gmail.com>
2015-08-01 19:38:02 -0700

    More work on doc site
    Temporarily prototype as a big component (hot reload!). Will split out
    css (or just stick with inline style) and other stuff later.

--- demos/demo6/Demo.jsx
+++ demos/demo6/Demo.jsx
@@ -48,7 +48,10 @@ const Demo = React.createClass({
       //   '}'
       // ].join('\n'));
     } catch (e) {
-      React.render(<div className="demo6-error">Something went wrong.{e.toString()}</div>, mountNode);
+      React.render(
+        <div className="demo6-error">Something went wrong.{e.message.split('\n')[0]}</div>,
+        mountNode
+      );
     }
   },
 
@@ -63,20 +66,79 @@ const Demo = React.createClass({
 
     return (
       <div>
-        <CodeMirror
-          value={code}
-          onChange={this.updateCode}
-          options={{
-            mode: 'javascript',
-            lineNumbers: true,
-            lineWrapping: true,
-            // javascript mode does bad things with jsx indents last time I checked
-            smartIndent: false,
-            matchBrackets: true,
-            theme: 'monokai',
+        <div className="nav-main" style={{
+          // display: 'flex',
+          width: '100%',
+          outline: '1px solid black',
+          display: 'flex',
+          justifyContent: 'center',
+          height: 50,
+        }}>
+          <div className="nav-inner" style={{
+            width: 920,
+            outline: '1px solid red',
+            display: 'flex',
+            justifyContent: 'flex-end',
+          }}>
+            <div style={{
+              padding: '0 10px',
+              lineHeight: '50px',
+            }}>
+              Docs
+            </div>
+            <div style={{
+              padding: '0 10px',
+              lineHeight: '50px',
+            }}>
+              Support
+            </div>
+            <div style={{
+              padding: '0 10px',
+              lineHeight: '50px',
+            }}>
+              Github
+            </div>
+          </div>
+        </div>
+
+        <div style={{
+          outline: '1px solid red',
+          paddingTop: 50,
+        }}>
+          <div ref="mountNode" style={{
+            display: 'flex',
+            justifyContent: 'center',
+            fontWeight: 400,
+            fontSize: '64px',
           }} />
 
-        <div ref="mountNode"></div>
+          <div style={{
+            textAlign: 'center',
+          }}>
+            A SPRING THAT SOLVES YOUR ANIMATION PROBLEMS
+          </div>
+
+
+          <div style={{
+            width: 600,
+            margin: '60px auto 0 auto',
+          }}>
+            <CodeMirror
+              value={code}
+              onChange={this.updateCode}
+              options={{
+                mode: 'javascript',
+                lineNumbers: true,
+                lineWrapping: true,
+                // javascript mode does bad things with jsx indents last time I checked
+                smartIndent: false,
+                matchBrackets: true,
+                theme: 'monokai',
+              }} />
+          </div>
+
+        </div>
+
       </div>
     );
   },
