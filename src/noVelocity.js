220a75bd9c7ca6494888848b75d3bee92137e40c
Cheng Lou <chenglou92@gmail.com>
2015-09-09 16:11:37 -0400

    Simplify noVelocity code
    No longer recursively drills down into an object while blacklisting
    everything but `config`.
    
    This simplification is so that we can have a (non-recursive,
    non-generic) noVelocity that treats some props, e.g. height or transform
    (an object maybe) differently in the future.

--- src/noVelocity.js
+++ src/noVelocity.js
@@ -1,13 +1,13 @@
-import isPlainObject from 'lodash.isplainobject';
-
-export default function noVelocity(coll) {
-  if (Array.isArray(coll)) {
-    return coll.every(noVelocity);
+// currentStyle keeps the info about whether a prop is configured as a spring
+// or if it's just a random prop that happens to be present on the style
+export default function noVelocity(currentVelocity, currentStyle) {
+  for (let key in currentVelocity) {
+    if (!currentVelocity.hasOwnProperty(key)) {
+      continue;
+    }
+    if (currentStyle[key].config && currentVelocity[key] !== 0) {
+      return false;
+    }
   }
-  if (isPlainObject(coll)) {
-    return Object.keys(coll).every(
-      key => key === 'config' ? true : noVelocity(coll[key])
-    );
-  }
-  return typeof coll === 'number' ? coll === 0 : true;
+  return true;
 }
