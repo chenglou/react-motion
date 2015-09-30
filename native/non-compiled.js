b1ccc08abc47ef60859528ba35b677dacfcda5d5
Cheng Lou <chenglou92@gmail.com>
2015-09-30 11:17:04 -0400

    Fix import for react-native

--- native/non-compiled.js
+++ native/non-compiled.js
@@ -2,7 +2,7 @@ import React from 'react-native';
 import components from '../lib/components';
 
 export const {Spring, TransitionSpring, Motion, StaggeredMotion, TransitionMotion} = components(React);
-export spring from './spring';
+export spring from '../lib/spring';
 export presets from '../lib/presets';
 import reorderKeys from '../lib/reorderKeys';
 export const utils = {
