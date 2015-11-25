a1caeb4337df04f22f02495c74b980af9b8d29a8
Cheng Lou <chenglou92@gmail.com>
2015-11-25 01:49:59 -0500

    Upgrade demos to use ReactDOM.render instead of React.render

--- demos/demo1-chat-heads/index.jsx
+++ demos/demo1-chat-heads/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import Demo from './Demo';
 
-React.render(<Demo />, document.querySelector('#content'));
+ReactDOM.render(<Demo />, document.querySelector('#content'));
