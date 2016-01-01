41e34f3e76ef46b8cc7ef2028e29d252d432024c
Cheng Lou <chenglou92@gmail.com>
2016-01-01 12:01:53 -0500

    Refine some more types
    (Local flow used by me is at version 0.20.1)
    Regarding the syntax of react-motion.js:
    https://github.com/facebook/flow/issues/940

--- src/Types.js
+++ src/Types.js
@@ -1,12 +1,17 @@
 /* @flow */
 
-export type Style = Object;
-export type Velocity = {
-  [key: string]: number,
-};
 export type SpringConfig = {
   val: number,
   config: [number, number],
+};
+export type Style = {
+  [key: string]: number | SpringConfig,
+};
+export type CurrentStyle = {
+  [key: string]: number,
+};
+export type Velocity = {
+  [key: string]: number,
 };
 export type TransitionStyles = {
   [key: string]: Style,
--- src/react-motion.js
+++ src/react-motion.js
@@ -1,9 +1,11 @@
+/* @flow */
+
 import React from 'react';
 import components from './components';
 
 export const {Spring, TransitionSpring, Motion, StaggeredMotion, TransitionMotion} = components(React);
-export spring from './spring';
-export presets from './presets';
+export {default as spring} from './spring';
+export {default as presets} from './presets';
 import reorderKeys from './reorderKeys';
 export const utils = {
   reorderKeys,
