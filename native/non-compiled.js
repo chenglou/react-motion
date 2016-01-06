ce63d0ced9874961956e5c1321cfd407ce35e5c2
Cheng Lou <chenglou92@gmail.com>
2016-01-05 23:18:19 -0500

    Import the new Motion component file
    This is just the imports. The commit itself, at this moment, would
    break, since there's no such file as makeMotion present.
    
    I'll check these in the next commit. This is just my desperate attempt
    at not committing big diffs.

--- native/non-compiled.js
+++ native/non-compiled.js
@@ -1,9 +1,12 @@
+/* @flow */
 import React from 'react-native';
 import components from '../lib/components';
+import {default as makeMotion} from '.,/lib/makeMotion';
 
-export const {Spring, TransitionSpring, Motion, StaggeredMotion, TransitionMotion} = components(React);
-export spring from '../lib/spring';
-export presets from '../lib/presets';
+export const Motion = makeMotion(React);
+export const {Spring, TransitionSpring, StaggeredMotion, TransitionMotion} = components(React);
+export {default as spring} from '../lib/spring';
+export {default as presets} from '../lib/presets';
 import reorderKeys from '../lib/reorderKeys';
 export const utils = {
   reorderKeys,
--- src/react-motion.js
+++ src/react-motion.js
@@ -1,9 +1,10 @@
 /* @flow */
-
 import React from 'react';
 import components from './components';
+import {default as makeMotion} from './makeMotion';
 
-export const {Spring, TransitionSpring, Motion, StaggeredMotion, TransitionMotion} = components(React);
+export const Motion = makeMotion(React);
+export const {Spring, TransitionSpring, StaggeredMotion, TransitionMotion} = components(React);
 export {default as spring} from './spring';
 export {default as presets} from './presets';
 import reorderKeys from './reorderKeys';
