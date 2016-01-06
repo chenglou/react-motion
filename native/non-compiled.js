687af95b8529dac5801270194af4f03d96a7c0d9
Cheng Lou <chenglou92@gmail.com>
2016-01-06 05:42:53 -0500

    Add correct import for the upcoming new TransitionMotion
    Actual implementation will follow.

--- native/non-compiled.js
+++ native/non-compiled.js
@@ -2,12 +2,14 @@
 import React from 'react-native';
 import components from '../lib/components';
 import {default as makeMotion} from '.,/lib/makeMotion';
-import {default as makeStaggeredMotion} from './makeStaggeredMotion';
+import {default as makeStaggeredMotion} from '../lib/makeStaggeredMotion';
+import {default as makeTransitionMotion} from '../lib/makeTransitionMotion';
 import reorderKeys from '../lib/reorderKeys';
 
 export const Motion = makeMotion(React);
 export const StaggeredMotion = makeStaggeredMotion(React);
-export const {Spring, TransitionSpring, TransitionMotion} = components(React);
+export const TransitionMotion = makeTransitionMotion(React);
+export const {Spring, TransitionSpring} = components(React);
 export {default as spring} from '../lib/spring';
 export {default as presets} from '../lib/presets';
 export const utils = {
--- src/react-motion.js
+++ src/react-motion.js
@@ -3,11 +3,13 @@ import React from 'react';
 import components from './components';
 import {default as makeMotion} from './makeMotion';
 import {default as makeStaggeredMotion} from './makeStaggeredMotion';
+import {default as makeTransitionMotion} from './makeTransitionMotion';
 import reorderKeys from './reorderKeys';
 
 export const Motion = makeMotion(React);
 export const StaggeredMotion = makeStaggeredMotion(React);
-export const {Spring, TransitionSpring, TransitionMotion} = components(React);
+export const TransitionMotion = makeTransitionMotion(React);
+export const {Spring, TransitionSpring} = components(React);
 export {default as spring} from './spring';
 export {default as presets} from './presets';
 export const utils = {
