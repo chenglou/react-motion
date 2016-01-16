45f581f007d87bf770bcfea85e53b28096744d7f
Cheng Lou <chenglou92@gmail.com>
2016-01-16 02:59:40 -0500

    Remove obsolete files & previous deprecation warnings
    Most of these aren't needed anymore after the previous commits'
    refactoring.
    
    components.js was also exporting deprecatedSprings.js Didn't feel like
    carrying this over so we'll remove the warnings too. It's been a few
    months already anyway. Next commit will remove the deprecated propTypes
    checks too.

--- src/react-motion.js
+++ src/react-motion.js
@@ -1,6 +1,5 @@
 /* @flow */
 import React from 'react';
-import components from './components';
 import {default as makeMotion} from './makeMotion';
 import {default as makeStaggeredMotion} from './makeStaggeredMotion';
 import {default as makeTransitionMotion} from './makeTransitionMotion';
@@ -9,7 +8,6 @@ import reorderKeys from './reorderKeys';
 export const Motion = makeMotion(React);
 export const StaggeredMotion = makeStaggeredMotion(React);
 export const TransitionMotion = makeTransitionMotion(React);
-export const {Spring, TransitionSpring} = components(React);
 export {default as spring} from './spring';
 export {default as presets} from './presets';
 export const utils = {
