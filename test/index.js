f1ce31c36577eb9893dde1857482519d6932dfb6
Nik Butenko <nik@butenko.me>
2016-01-16 19:50:07 +1100

    Remove prettyMatcher

--- test/index.js
+++ test/index.js
@@ -1,7 +1,3 @@
-import prettyMatcher from './prettyMatcher';
-
-beforeEach(() => jasmine.addMatchers({toEqual: prettyMatcher}));
-
 const testsContext = require.context('./', true, /\-test\.js$/);
 
 testsContext.keys().forEach(testsContext);
