1ab36c148258469a5d254ad4f5bd7a8ecee3ac34
Cheng Lou <chenglou92@gmail.com>
2016-01-22 23:21:11 -0500

    Remove reorderKeys
    See previous commits: TransitionMotion now uses array styles instead of
    object styles. Reorderkey is therefore no longer needed. If you want to
    have this neat little utility exposed, feel free to leave a message in
    the comments here, and I'll put this into its own library.

--- src/react-motion.js
+++ src/react-motion.js
@@ -3,13 +3,9 @@ import React from 'react';
 import makeMotion from './makeMotion';
 import makeStaggeredMotion from './makeStaggeredMotion';
 import makeTransitionMotion from './makeTransitionMotion';
-import reorderKeys from './reorderKeys';
 
 export const Motion = makeMotion(React);
 export const StaggeredMotion = makeStaggeredMotion(React);
 export const TransitionMotion = makeTransitionMotion(React);
 export {default as spring} from './spring';
 export {default as presets} from './presets';
-export const utils = {
-  reorderKeys,
-};
