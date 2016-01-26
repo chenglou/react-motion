1cf9ef4a95000ef8b173fdb5a74e9e54597b8e33
Cheng Lou <chenglou92@gmail.com>
2016-01-25 23:02:20 -0500

    Small comment refinement for presets.js

--- src/presets.js
+++ src/presets.js
@@ -1,6 +1,6 @@
 /* @flow */
 export default {
-  noWobble: {stiffness: 170, damping: 26}, // the default
+  noWobble: {stiffness: 170, damping: 26}, // the default, if nothing provided
   gentle: {stiffness: 120, damping: 14},
   wobbly: {stiffness: 180, damping: 12},
   stiff: {stiffness: 210, damping: 20},
