7e6f942ad97cf43c62ba2b87d855c88e278eb913
Cheng Lou <chenglou92@gmail.com>
2016-01-30 00:53:29 -0500

    Make mergeDiff tests more robust
    - Add helper to make the tests much more concise.
    - Automate a few checks.
    - Fully commented.
    - Add test cases.
    
    The last one breaks the existing implementation. Will fix in the next
    few commits.

--- test/mergeDiff-test.js
+++ test/mergeDiff-test.js
@@ -2,89 +2,81 @@ import mergeDiff from '../src/mergeDiff';
 
 const id = (_, s) => s;
 const n = () => null;
+
+// helper to make the tests more concise
+function test(prevRaw, nextRaw, expectedRaw, customOnRemove) {
+  // we elaborately construct prev/nextKeyStyleValMap + randomized style value to
+  // check that the style object of the latter correctly merged into the final
+  // output
+  let prev = [];
+  let prevKeyStyleValMap = {};
+  prevRaw.forEach(num => {
+    const styleVal = Math.random();
+    // key needs to be a string; cast it
+    prev.push({key: num + '', style: {a: styleVal}});
+    prevKeyStyleValMap[num] = styleVal;
+  });
+  let next = [];
+  let nextKeyStyleValMap = {};
+  nextRaw.forEach(num => {
+    const styleVal = Math.random();
+    next.push({key: num + '', style: {a: styleVal}});
+    nextKeyStyleValMap[num] = styleVal;
+  });
+
+  const expected = expectedRaw.map(num => {
+    return {
+      key: num + '',
+      style: {a: nextKeyStyleValMap.hasOwnProperty(num) ? nextKeyStyleValMap[num] : prevKeyStyleValMap[num]},
+    };
+  });
+
+  expect(mergeDiff(prev, next, n)).toEqual(next);
+  // some tests pass in a `customOnRemove` to check edge cases; interpret
+  // `expected`/`expectedRaw` as the output of mergeDiff using `customOnRemove`
+  // instead of the default `id` function
+  expect(mergeDiff(prev, next, customOnRemove || id)).toEqual(expected);
+}
+
 describe('mergeDiff', () => {
-  it('should merge latter key value into former', () => {
-    expect(mergeDiff([{key: 1, style: {a: 1}}], [{key: 1, style: {a: 2}}], id))
-      .toEqual([{key: 1, style: {a: 2}}]);
-    expect(mergeDiff([{key: 1, style: {a: 1}}], [{key: 1, style: {a: 2}}], n))
-      .toEqual([{key: 1, style: {a: 2}}]);
+  it('should work with various merge orders', () => {
+    // most of these tests are significant. Don't casually remove some. Those
+    // marked as "meh" are the ones whose order can differ. We've chosen a
+    // deterministic default in our mergeDiff implementation
+    test([4], [], [4]);
+    test([], [3], [3]);
+    test([3], [3], [3]);
+    test([], [], []);
+    test([2, 4, 5, 6], [2, 3, 4, 5], [2, 3, 4, 5, 6]);
+    test([2, 4, 5, 6, 7], [1, 2, 3, 4, 5], [1, 2, 3, 4, 5, 6, 7]);
+    test([1, 2, 3], [2, 3, 4], [1, 2, 3, 4]);
+    test([2, 3, 4], [1, 2, 3], [1, 2, 3, 4]);
+    test([4], [1, 2, 3], [4, 1, 2, 3]); // meh
+    test([1, 2, 3], [4], [1, 2, 3, 4]); // meh
+    test([4, 2], [1, 2, 3], [4, 1, 2, 3]); // meh
+    test([2, 4], [1, 2, 3], [1, 2, 4, 3]); // meh
+    test([1, 5, 10], [3, 5, 7, 10], [1, 3, 5, 7, 10]); // meh
+    test([4, 5, 10], [3, 5, 7, 10], [4, 3, 5, 7, 10]); // meh
+    test([4], [3], [4, 3]); // meh
+    test([1, 5], [5, 3], [1, 5, 3]);
+    test([5, 6], [3, 5], [3, 5, 6]);
+    test([1, 2, 3], [3, 2, 1], [3, 2, 1]);
+    test([3, 2, 1], [1, 2, 3], [1, 2, 3]);
+    test([1, 2, 3], [2, 1, 3], [2, 1, 3]);
+    test([1, 2, 3], [1, 3, 2], [1, 3, 2]);
+    test([1, 2, 3], [1, 2, 3], [1, 2, 3]);
   });
 
-  it('should reorder keys according to latter', () => {
-    expect(mergeDiff(
-      [{key: 1, style: {a: 1}}, {key: 2, style: {a: 2}}],
-      [{key: 2, style: {a: 3}}, {key: 1, style: {a: 4}}],
-      id,
-    )).toEqual([{key: 2, style: {a: 3}}, {key: 1, style: {a: 4}}]);
-
-    expect(mergeDiff(
-      [{key: 1, style: {a: 1}}, {key: 2, style: {a: 2}}],
-      [{key: 2, style: {a: 3}}, {key: 1, style: {a: 4}}],
-      n,
-    )).toEqual([{key: 2, style: {a: 3}}, {key: 1, style: {a: 4}}]);
-  });
-
-  it('should remove key if cb returns nullable, else updated value', () => {
-    expect(mergeDiff(
-      [{key: 1, style: {a: 1}}, {key: 2, style: {a: 2}}],
-      [{key: 2, style: {a: 3}}],
-      id,
-    )).toEqual([{key: 1, style: {a: 1}}, {key: 2, style: {a: 3}}]);
-
-    expect(mergeDiff(
-      [{key: 2, style: {a: 1}}, {key: 1, style: {a: 2}}],
-      [{key: 2, style: {a: 3}}, {key: 3, style: {a: 4}}],
-      id,
-    )).toEqual([
-      {key: 2, style: {a: 3}},
-      {key: 1, style: {a: 2}},
-      {key: 3, style: {a: 4}},
-    ]);
-
-    expect(mergeDiff(
-      [{key: 1, style: {a: 1}}, {key: 2, style: {a: 2}}],
-      [{key: 2, style: {a: 3}}],
-      n,
-    )).toEqual([{key: 2, style: {a: 3}}]);
-  });
-
-  it('should not consider move as a delete', () => {
-    // a few cases covered by "reorder keys..." above
-    expect(mergeDiff(
-      [{key: 1, style: {a: 1}}, {key: 2, style: {a: 2}}, {key: 3, style: {a: 3}}],
-      [{key: 3, style: {a: 4}}, {key: 2, style: {a: 5}}],
-      n,
-    )).toEqual([{key: 3, style: {a: 4}}, {key: 2, style: {a: 5}}]);
-  });
-
-  it('should do all of these together', () => {
-    expect(mergeDiff(
-      [{key: 1, style: {a: 1}}, {key: 2, style: {a: 2}}, {key: 3, style: {a: 3}}],
-      [{key: 1, style: {a: 4}}, {key: 9, style: {a: 9}}],
-      (index, s) => index === 1 ? {...s, style: {a: 10}} : null,
-    )).toEqual([
-      {key: 1, style: {a: 4}},
-      {key: 2, style: {a: 10}},
-      {key: 9, style: {a: 9}},
-    ]);
-
-    expect(mergeDiff(
-      [{key: 1, style: {a: 1}}, {key: 2, style: {a: 2}}, {key: 3, style: {a: 3}}, {key: 4, style: {a: 4}}],
-      [{key: 5, style: {a: 5}}, {key: 4, style: {a: 6}}, {key: 2, style: {a: 7}}],
-      (index, s) => index === 0 ? {...s, style: {a: 10}} : null,
-    )).toEqual([
-      {key: 1, style: {a: 10}},
-      {key: 5, style: {a: 5}},
-      {key: 4, style: {a: 6}},
-      {key: 2, style: {a: 7}},
-    ]);
+  it('should work with some more typical onRemove callbacks', () => {
+    test([1, 2, 3], [1, 9], [1, 2, 9], (index, s) => index === 1 ? s : null);
+    test([1, 2, 3, 4], [5, 4, 2], [1, 5, 4, 2], (index, s) => index === 0 ? s : null);
   });
 
   it('should not call cb more than once per disappearing key', () => {
     let count = 0;
-    mergeDiff([{key: 1, style: {a: 1}}], [], () => {
+    test([1], [], [], () => {
       count++;
-      return {key: 1, style: {}};
+      return null;
     });
     expect(count).toBe(1);
   });
