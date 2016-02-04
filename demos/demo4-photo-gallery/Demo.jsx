078813f87815a0a636b897bd42c33d2076f255e2
Samar Panda <er.samarpanda@gmail.com>
2016-02-04 07:35:42 +0530

    Adding button and its interaction to show next items
    Use bind for next and previous button clickHandler

--- demos/demo4-photo-gallery/Demo.jsx
+++ demos/demo4-photo-gallery/Demo.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {Motion, spring} from '../../src/react-motion';
 
 const springSettings = {stiffness: 170, damping: 26};
+const NEXT = 'show-next';
+
 const Demo = React.createClass({
   getInitialState() {
     return {
@@ -12,6 +14,17 @@ const Demo = React.createClass({
 
   handleChange({target: {value}}) {
     this.setState({currPhoto: value});
+  },
+
+  clickHandler(btn){
+    var photoIndex = btn === NEXT ? this.state.currPhoto+1 : this.state.currPhoto-1;
+
+    photoIndex = photoIndex >= 0 ? photoIndex : this.state.photos.length - 1;
+    photoIndex = photoIndex >= this.state.photos.length ? 0 : photoIndex;
+
+    this.setState({
+      currPhoto: photoIndex
+    })
   },
 
   render() {
@@ -37,12 +50,14 @@ const Demo = React.createClass({
     return (
       <div>
         <div>Scroll Me</div>
+        <button onClick={this.clickHandler.bind(null, '')}>Previous</button>
         <input
           type="range"
           min={0}
           max={photos.length - 1}
           value={currPhoto}
           onChange={this.handleChange} />
+        <button onClick={this.clickHandler.bind(null, NEXT)}>Next</button>
         <div className="demo4">
           <Motion style={{height: spring(currHeight), width: spring(currWidth)}}>
             {container =>
