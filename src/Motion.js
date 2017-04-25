08da4f3e4e7ab8e15db5bb0b3a1c647fbe7c0216
Trevor <ifndefdeadmau5@gmail.com>
2017-04-25 14:48:55 +0900

    Use ES6 classes in ‘Motion.js’

--- src/Motion.js
+++ src/Motion.js
@@ -5,9 +5,8 @@ import stepper from './stepper';
 import defaultNow from 'performance-now';
 import defaultRaf from 'raf';
 import shouldStopAnimation from './shouldStopAnimation';
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import createReactClass from 'create-react-class';
 
 import type {ReactElement, PlainStyle, Style, Velocity, MotionProps} from './Types';
 
@@ -20,8 +19,8 @@ type MotionState = {
   lastIdealVelocity: Velocity,
 };
 
-const Motion = createReactClass({
-  propTypes: {
+export default class Motion extends Component {
+  static propTypes = {
     // TOOD: warn against putting a config in here
     defaultStyle: PropTypes.objectOf(PropTypes.number),
     style: PropTypes.objectOf(PropTypes.oneOfType([
@@ -30,9 +29,17 @@ const Motion = createReactClass({
     ])).isRequired,
     children: PropTypes.func.isRequired,
     onRest: PropTypes.func,
-  },
+  };
 
-  getInitialState(): MotionState {
+  state: MotionState;
+  props: MotionProps;
+
+  constructor(props: MotionProps) {
+    super(props);
+    this.state = this.defaultState();
+  }
+
+  defaultState(): MotionState {
     const {defaultStyle, style} = this.props;
     const currentStyle = defaultStyle || stripStyle(style);
     const currentVelocity = mapToZero(currentStyle);
@@ -42,22 +49,22 @@ const Motion = createReactClass({
       lastIdealStyle: currentStyle,
       lastIdealVelocity: currentVelocity,
     };
-  },
+  }
 
-  wasAnimating: false,
-  animationID: (null: ?number),
-  prevTime: 0,
-  accumulatedTime: 0,
+  wasAnimating: false;
+  animationID: ?number = null;
+  prevTime = 0;
+  accumulatedTime= 0;
   // it's possible that currentStyle's value is stale: if props is immediately
   // changed from 0 to 400 to spring(0) again, the async currentStyle is still
   // at 0 (didn't have time to tick and interpolate even once). If we naively
   // compare currentStyle with destVal it'll be 0 === 0 (no animation, stop).
   // In reality currentStyle should be 400
-  unreadPropStyle: (null: ?Style),
+  unreadPropStyle: ?Style = null;
   // after checking for unreadPropStyle != null, we manually go set the
   // non-interpolating values (those that are a number, without a spring
   // config)
-  clearUnreadPropStyle(destStyle: Style): void {
+  clearUnreadPropStyle = (destStyle: Style): void => {
     let dirty = false;
     let {currentStyle, currentVelocity, lastIdealStyle, lastIdealVelocity} = this.state;
 
@@ -86,9 +93,9 @@ const Motion = createReactClass({
     if (dirty) {
       this.setState({currentStyle, currentVelocity, lastIdealStyle, lastIdealVelocity});
     }
-  },
+  };
 
-  startAnimationIfNecessary(): void {
+  startAnimationIfNecessary = (): void => {
     // TODO: when config is {a: 10} and dest is {a: 10} do we raf once and
     // call cb? No, otherwise accidental parent rerender causes cb trigger
     this.animationID = defaultRaf((timestamp) => {
@@ -198,37 +205,35 @@ const Motion = createReactClass({
 
       this.startAnimationIfNecessary();
     });
-  },
+  };
 
   componentDidMount() {
     this.prevTime = defaultNow();
     this.startAnimationIfNecessary();
-  },
+  }
 
   componentWillReceiveProps(props: MotionProps) {
-    if (this.unreadPropStyle != null) {
+    if (this.unreadPropStyle !== null) {
       // previous props haven't had the chance to be set yet; set them here
       this.clearUnreadPropStyle(this.unreadPropStyle);
     }
 
     this.unreadPropStyle = props.style;
-    if (this.animationID == null) {
+    if (this.animationID === null) {
       this.prevTime = defaultNow();
       this.startAnimationIfNecessary();
     }
-  },
+  }
 
   componentWillUnmount() {
-    if (this.animationID != null) {
+    if (this.animationID !== null) {
       defaultRaf.cancel(this.animationID);
       this.animationID = null;
     }
-  },
+  }
 
   render(): ReactElement {
     const renderedChildren = this.props.children(this.state.currentStyle);
     return renderedChildren && React.Children.only(renderedChildren);
-  },
-});
-
-export default Motion;
+  }
+}
