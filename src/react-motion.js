2c327c1992673287a8c891150e6fe0ce8e2ab66e
Kevin Barabash <kevinb@khanacademy.org>
2021-01-12 13:08:34 -0500

    customize the build for KA

--- src/react-motion.js
+++ src/react-motion.js
@@ -1,10 +1,11 @@
 /* @flow */
 export { default as Motion } from './Motion';
-export { default as StaggeredMotion } from './StaggeredMotion';
-export { default as TransitionMotion } from './TransitionMotion';
+// export { default as StaggeredMotion } from './StaggeredMotion';
+// TODO(FEI-3195): enable this once the ADR has been approved
+// export { default as TransitionMotion } from './TransitionMotion';
 export { default as spring } from './spring';
 export { default as presets } from './presets';
-export { default as stripStyle } from './stripStyle';
+// export { default as stripStyle } from './stripStyle';
 
 // deprecated, dummy warning function
-export { default as reorderKeys } from './reorderKeys';
+// export { default as reorderKeys } from './reorderKeys';
